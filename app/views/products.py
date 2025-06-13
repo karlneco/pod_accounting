9c1c7e1fb484d862652344e86e811aba413ac5ca
Karl Kowalczyk <karl1112@gmail.com>
2025-06-13 10:46:10 -0600

    added skeleton products section of the site

--- app/views/products.py
+++ app/views/products.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from ..models import db, Customer
+from flask import Blueprint, render_template, url_for, redirect, flash
+from sqlalchemy import func
+from ..models import db, Product, OrderItem
 
-bp = Blueprint(
-    "products",
-    __name__,
-    template_folder="templates/products"
-)
+bp = Blueprint('products', __name__, template_folder='templates/products')
+
+@bp.route('/')
+def list_products():
+    # For each product, sum up quantities sold (0 if none)
+    stats = (
+        db.session.query(
+            Product,
+            func.coalesce(func.sum(OrderItem.quantity), 0).label('items_sold')
+        )
+        .outerjoin(OrderItem)
+        .group_by(Product.id)
+        .order_by(Product.name)
+        .all()
+    )
+    total_products = len(stats)
+    return render_template(
+        'products/list.html',
+        stats=stats,
+        total_products=total_products
+    )
+
+@bp.route('/new')
+def create_product():
+    flash('Product creation not implemented yet', 'info')
+    return redirect(url_for('products.list_products'))
+
+@bp.route('/import')
+def import_products():
+    flash('Product import not implemented yet', 'info')
+    return redirect(url_for('products.list_products'))
